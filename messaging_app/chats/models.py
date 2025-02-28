54fb0ba4a057abea2e99cbad3954f47158104bea
TewodrosAdimas <yosefenadagim@gmail.com>
2025-02-28 08:45:59 +0300

    models created

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(CustomUser, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} at {self.timestamp}"
