c90566721b4f8e3d28cf321a2900ecc326ba614e
TewodrosAdimas <yosefenadagim@gmail.com>
2025-02-28 14:09:44 +0300

    users registration view added

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -10,6 +10,31 @@ from .permissions import IsConversationParticipant, IsMessageSenderOrConversatio
 from rest_framework.pagination import PageNumberPagination
 from django_filters import rest_framework as filters
 from .filters import MessageFilter  # Import the filter class
+from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+class RegisterView(APIView):
+    """
+    API endpoint for user registration.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "user": serializer.data,
+                "access": str(refresh.access_token),
+                "refresh": str(refresh)
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class MessagePagination(PageNumberPagination):
     page_size = 20  # Number of messages per page
