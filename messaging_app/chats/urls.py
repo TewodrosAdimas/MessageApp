53abc8f0514f8709b354504d7481c1a68840e5b5
TewodrosAdimas <yosefenadagim@gmail.com>
2025-02-28 14:24:28 +0300

    url path modified

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from chats.views import ConversationViewSet, MessageViewSet,RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    LoginView,
+)
+from chats.views import ConversationViewSet, MessageViewSet, RegisterView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
+router.register(r"conversations", ConversationViewSet)
+router.register(r"messages", MessageViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/", include(router.urls)),
+    path("login/", LoginView.as_view(), name="login"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
