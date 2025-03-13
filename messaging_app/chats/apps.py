14d127cbb5888962c93d12bf2f3e13d777e6fbda
TewodrosAdimas <yosefenadagim@gmail.com>
2025-03-13 09:55:11 +0300

    Register Signals in apps.py

--- messaging_app/chats/apps.py
+++ messaging_app/chats/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ChatsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "chats"
+
+class ChatAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "chats"
+
+    def ready(self):
+        import chats.signals
