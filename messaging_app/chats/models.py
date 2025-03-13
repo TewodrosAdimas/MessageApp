0f87b420619a068cd707961cf3e14dfd222be1dc
TewodrosAdimas <yosefenadagim@gmail.com>
2025-03-13 09:53:10 +0300

    notification model added

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -33,3 +33,11 @@ class Message(models.Model):
     def __str__(self):
         return f"Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}"
     
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="notifications")
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username} - Message {self.message.id}"
