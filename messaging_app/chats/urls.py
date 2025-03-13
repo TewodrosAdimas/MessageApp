1962addb9d3a77354998bf2bf8324963c7ca1d3c
TewodrosAdimas <yosefenadagim@gmail.com>
2025-03-13 10:45:10 +0300

    url modified

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -19,6 +19,10 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('message/<int:message_id>/history/', views.message_history, name='message_history'),
-
+    path(
+        "message/<int:message_id>/history/",
+        views.message_history,
+        name="message_history",
+    ),
+    path("delete-account/", views.delete_user, name="delete_user"),
 ]
